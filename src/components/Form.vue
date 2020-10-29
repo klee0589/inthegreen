dcf7b26614b2544cd46482435c29b384f2dfe0d2
Kangil Lee <klee0589@gmail.com>
2020-10-29 14:46:12 -0400

    added basic form

---
+++ src/components/Form.vue
@@ -0,0 +1,74 @@
+<template>
+  <b-container fluid>
+    <b-form @submit="onSubmit" @reset="onReset">
+      <b-row>
+        <b-col cols="2">
+          <b-form-group
+            id="input-group-1"
+            label="Stake:"
+            description="How much you betting?"
+          >
+            <b-input-group size="md" prepend="$" append=".00">
+              <b-form-input v-model="form.stake"></b-form-input>
+            </b-input-group> </b-form-group
+        ></b-col>
+        <b-col cols="2">
+          <b-form-group id="input-group-4" label="+/-">
+            <b-form-checkbox-group v-model="form.checked" id="checkboxes-4">
+              <b-form-checkbox value="+">+</b-form-checkbox>
+              <b-form-checkbox value="-">-</b-form-checkbox>
+            </b-form-checkbox-group>
+          </b-form-group>
+        </b-col>
+        <b-col cols="2">
+          <b-form-group
+            id="input-group-1"
+            label="Odds:"
+            label-for="input-1"
+            description="Odds on bet"
+          >
+            <b-form-input
+              id="input-1"
+              v-model="form.odds"
+              type="number"
+              required
+              placeholder="ex: 300"
+            ></b-form-input> </b-form-group
+        ></b-col>
+      </b-row>
+
+      <b-button type="submit" variant="primary">Submit</b-button>
+      <b-button type="reset" variant="danger">Reset</b-button>
+    </b-form>
+    <b-card class="mt-3" header="Form Data Result">
+      <pre class="m-0">{{ form }}</pre>
+    </b-card>
+  </b-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        stake: "",
+        odds: "",
+        checked: []
+      }
+    };
+  },
+  methods: {
+    onSubmit(evt) {
+      evt.preventDefault();
+      alert(JSON.stringify(this.form));
+    },
+    onReset(evt) {
+      evt.preventDefault();
+      // Reset our form values
+      this.form.stakes = "";
+      this.form.odds = "";
+      this.form.checked = [];
+    }
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+    <Form />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import Form from "@/components/Form.vue"; // @ is an alias to /src
 
 @Component({
   components: {
-    HelloWorld
+    Form
   }
 })
 export default class Home extends Vue {}
