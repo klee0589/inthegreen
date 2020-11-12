373f918a182f07394297208a0f9dd51b470007b1
Kangil Lee <klee0589@gmail.com>
2020-11-12 00:44:33 -0500

    update route name

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
-import Home from "../views/Home.vue";
+import Odds from "../views/Odds.vue";
 import Sports from "../views/Sports.vue";
 import Lotto from "../views/Lotto.vue";
 
@@ -9,8 +9,8 @@ Vue.use(VueRouter);
 const routes: Array<RouteConfig> = [
   {
     path: "/",
-    name: "Home",
-    component: Home
+    name: "Odds",
+    component: Odds
   },
   {
     path: "/sports",
