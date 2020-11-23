839f901b4cbeeb4a7fe0d30f2f0f7d0cceeaf720
Kangil Lee <klee0589@gmail.com>
2020-11-23 16:12:42 -0500

    updated odd board

--- src/components/BetBoard.vue
+++ src/components/BetBoard.vue
@@ -1,14 +1,10 @@
 <template>
   <div>
     <b-row>
-      <b-col
-        cols="12"
-        sm="6"
-        med="6"
-        v-for="game in odds"
-        :key="game.home_team"
-      >
-        <GameInfo :game="game" />
+      <b-col cols="12" sm="6" med="6" v-for="game in odds" :key="game.teams">
+        <!-- Using value -->
+        <b-button v-b-modal="game.teams">{{ game.teams }}</b-button>
+        <b-modal :id="game.teams"><GameInfo :game="game" /></b-modal>
       </b-col>
     </b-row>
   </div>
@@ -21,12 +17,12 @@ import GameInfo from "./GameInfo";
 export default {
   data() {
     return {
-      selected: null
+      selected: null,
     };
   },
   computed: mapState({
     // arrow functions can make the code very succinct!
-    odds: state => state.odds
+    odds: (state) => state.odds
 
     // // passing the string value 'count' is same as `state => state.count`
     // countAlias: 'count',
@@ -37,8 +33,8 @@ export default {
     // }
   }),
   components: {
-    GameInfo
-  }
+    GameInfo,
+  },
 };
 </script>
 
