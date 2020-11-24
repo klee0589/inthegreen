faa13180c500340949c83c22b5de7b96e93cd4bd
Kangil Lee <klee0589@gmail.com>
2020-11-24 15:35:53 -0500

    wip

--- src/router/index.ts
+++ src/router/index.ts
@@ -22,7 +22,12 @@ const routes: Array<RouteConfig> = [
     path: "/lotto",
     name: "Lotto",
     component: Lotto
-  }
+  },
+  {
+    path: "/odds",
+    name: "Odds",
+    component: Odds
+  },
 ];
 
 const router = new VueRouter({
